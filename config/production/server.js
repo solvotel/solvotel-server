8deca9142f0e6812b15f57bed70cefb47e0c0e4d
Amit Mahapatra <connectamit23@gmail.com>
2025-10-17 05:36:24 +0530

    aa

--- config/production/server.js
+++ config/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://82.29.164.139'), // change to your domain/IP
+  url: env('PUBLIC_URL', 'https://88.222.245.103'), // change to your domain/IP
   app: { keys: env.array('APP_KEYS') },
 });
